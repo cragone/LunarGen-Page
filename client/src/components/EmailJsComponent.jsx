e705059c8e2b58d125eb75bf8de3a4156d3f3f4c
cragone <ragonecharlie@gmail.com>
2024-04-11 12:42:45 -0400

    now clears contact form and sends alert

--- client/src/components/EmailJsComponent.jsx
+++ client/src/components/EmailJsComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef} from 'react';
 import emailjs from '@emailjs/browser';
 
 
@@ -16,6 +16,8 @@ const EmailJsComponent = () => {
             .then(
                 () => {
                     console.log('SUCCESS!');
+                    form.current.reset();
+                    alert("Successfully Sent!")
                 },
                 (error) => {
                     console.log('FAILED...', error.text);
@@ -23,22 +25,23 @@ const EmailJsComponent = () => {
             );
     };
 
+
     return (
-        <div className="flex items-center justify-center h-screen bg-gray-800">
-                <div className="card bordered bg-gray-600 w-1/3">
-                    <div className="card-body">
-                        <h2 className="card-title text-black">Contact Us</h2>
-                        <form className="form-control" ref={form} onSubmit={sendEmail}>
-                            <label className="label label-text text-black">Name</label>
-                            <input type="text" className="input input-bordered text-black bg-gray-200" name="user_name" />
-                            <label className="label label-text text-black">Email</label>
-                            <input type="email" className="input input-bordered text-black bg-gray-200" name="user_email" />
-                            <label className="label label-text text-black">Message</label>
-                            <textarea name="message" className="input input-bordered text-black bg-gray-200" />
-                            <input type="submit" value="Send" className="btn btn-black mt-4 hover:bg-primary" />
-                        </form>
-                    </div>
+        <div className="flex items-center justify-center h-screen bg-gray-800 relative">
+            <div className="card bordered bg-gray-600 h-1/3 w-1/3">
+                <div className="card-body">
+                    <h2 className="card-title text-black">Contact Us</h2>
+                    <form className="form-control" ref={form} onSubmit={sendEmail}>
+                        <label className="label label-text text-black">Name</label>
+                        <input type="text" className="input input-bordered text-black bg-gray-200" name="user_name" />
+                        <label className="label label-text text-black">Email</label>
+                        <input type="email" className="input input-bordered text-black bg-gray-200" name="user_email" />
+                        <label className="label label-text text-black">Message</label>
+                        <textarea name="message" className="input input-bordered text-black bg-gray-200" />
+                        <input type="submit" value="Send" className="btn btn-black mt-4 hover:bg-primary" />
+                    </form>
                 </div>
+            </div>
         </div>
     );
 };
